c30ecb4cbd0b6ec5f0a9026403a680bd29d52174
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2016-04-25 15:53:06 +0300

    MainWindow: Add ability to show/hide the notebook.

--- gtimecalc/gtimecalc.py
+++ gtimecalc/gtimecalc.py
@@ -1,5 +1,6 @@
 
 from gettext import gettext as _
+import struct
 
 from gi.repository import Gtk, Gdk, Gio, GLib
 
@@ -8,6 +9,9 @@ from .settings import settings
 from .common import WIDGET_SPACING
 from .calculator import Calculator
 from .notebook import Notebook
+
+
+_C_INT_MAX = 2 ** (struct.Struct('i').size * 8 - 1) - 1
 
 
 class MainWindow(Gtk.ApplicationWindow):
@@ -36,6 +40,15 @@ class MainWindow(Gtk.ApplicationWindow):
 
         grid.show_all()
 
+        notebook_visible_action = Gio.SimpleAction.new_stateful(
+            'notebook-visible',
+            None,
+            GLib.Variant.new_boolean(self._notebook.get_visible()),
+            )
+        notebook_visible_action.connect(
+            'change-state', self._on_notebook_visible_toggle)
+        self.add_action(notebook_visible_action)
+
     def _create_menubar(self):
         ag = Gtk.AccelGroup()
         self.add_accel_group(ag)
@@ -56,6 +69,17 @@ class MainWindow(Gtk.ApplicationWindow):
             'activate', ag, key, mod, Gtk.AccelFlags.VISIBLE)
         file_menu.append(mi_quit)
 
+        # View
+        view_menu = Gtk.Menu()
+        mi_view = Gtk.MenuItem.new_with_mnemonic(_('_View'))
+        mi_view.set_submenu(view_menu)
+        menubar.append(mi_view)
+
+        mi_show_equations = Gtk.CheckMenuItem(
+            label=_('Show Equations'),
+            action_name='win.notebook-visible')
+        view_menu.append(mi_show_equations)
+
         # Help
         help_menu = Gtk.Menu()
         mi_help = Gtk.MenuItem.new_with_mnemonic(_('_Help'))
@@ -69,6 +93,21 @@ class MainWindow(Gtk.ApplicationWindow):
 
         return menubar
 
+    def _udpate_geometry_hints(self):
+        geometry = Gdk.Geometry()
+        geometry.max_width = _C_INT_MAX
+        if self._notebook.get_visible():
+            geometry.max_height = _C_INT_MAX
+        else:
+            geometry.max_height = -1
+
+        self.set_geometry_hints(None, geometry, Gdk.WindowHints.MAX_SIZE)
+
+    def _on_notebook_visible_toggle(self, action, value):
+        action.set_state(value)
+        self._notebook.set_visible(value.get_boolean())
+        self._udpate_geometry_hints()
+
     def do_window_state_event(self, event):
         self._maximized = bool(
             event.new_window_state & Gdk.WindowState.MAXIMIZED)
@@ -80,9 +119,15 @@ class MainWindow(Gtk.ApplicationWindow):
 
     def save_state(self):
         state = {}
+
+        notebook_visible_state = self.get_action_state('notebook-visible')
+        state['show_equations'] = notebook_visible_state.get_boolean()
+        notebook_visible_state.unref()
+
         state['x'], state['y'] = self.get_position()
         state['width'], state['height'] = self.get_size()
         state['maximized'] = self._maximized
+
         settings['window'] = state
 
         self._calculator.save_state()
@@ -101,6 +146,10 @@ class MainWindow(Gtk.ApplicationWindow):
                 self.move(state['x'], state['y'])
                 self.resize(
                     state['width'], state['height'])
+
+            self.change_action_state(
+                'notebook-visible',
+                GLib.Variant.new_boolean(state['show_equations']))
         except (KeyError, TypeError):
             pass
 
