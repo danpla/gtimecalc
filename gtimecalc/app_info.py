537840d5033568d50b3db2d7f472dc904827c07e
Daniel Plakhotich <daniel.plakhotich@gmail.com>
2019-03-31 21:03:11 +0300

    Update copyright year

--- gtimecalc/app_info.py
+++ gtimecalc/app_info.py
@@ -1,5 +1,5 @@
 '''
-Copyright © 2016-2018 Daniel Plakhotich
+Copyright © 2016-2019 Daniel Plakhotich
 
 This software is provided 'as-is', without any express or implied
 warranty. In no event will the authors be held liable for any damages
@@ -24,7 +24,7 @@ VERSION = '1.0.2'
 NAME = TITLE.lower()
 ICON = NAME
 
-COPYRIGHT = 'Copyright © 2016-2018 Daniel Plakhotich'
+COPYRIGHT = 'Copyright © 2016-2019 Daniel Plakhotich'
 LICENSE = __doc__
 
 WEBSITE = 'https://danpla.github.io/gtimecalc/'
